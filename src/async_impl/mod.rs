8f9b1314231a6128810564b97dad5a8540a14476
twissel <twissel@gmail.com>
2017-09-06 16:54:58 +0300

    Update mod.rs

--- src/async_impl/mod.rs
+++ src/async_impl/mod.rs
@@ -2,8 +2,8 @@
 
 pub use self::body::{Body, Chunk};
 pub use self::decoder::{Decoder, ReadableChunks};
-pub use self::client::{Client, ClientBuilder};
-pub use self::request::{Request, RequestBuilder, Pending};
+pub use self::client::{Client, ClientBuilder, Pending};
+pub use self::request::{Request, RequestBuilder};
 pub use self::response::Response;
 
 pub mod body;
